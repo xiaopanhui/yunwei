1de5007641f46e0a4525e5b10767503a4ea3e342
xiezhongtao <example@email.com>
2018-09-30 12:02:18 +0800

    更新权限管理

--- src/main/java/com/gzsf/operation/ResponseUtils.java
+++ src/main/java/com/gzsf/operation/ResponseUtils.java
@@ -7,5 +7,7 @@ public class ResponseUtils {
     public static <T> Response<T> noUser(){return new Response<T>(100,"No User");}
     public static <T> Response<T> accessDenied(){return new Response<T>(403,"Access Denied");}
     public static <T> Response<T> systemError(){return new Response<T>(500,"System Error");}
+    public static <T> Response<T> notFound(){return new Response<T>(404,"Not Found");}
+    public static <T> Response<T> notLogin(){return new Response<T>(101,"Not Login");}
 
 }
--- src/main/java/com/gzsf/operation/controller/GlobalExceptionHandler.java
+++ src/main/java/com/gzsf/operation/controller/GlobalExceptionHandler.java
@@ -8,12 +8,13 @@ import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.server.HttpServerRequest;
 
+import org.springframework.security.access.AccessDeniedException;
+import java.util.Map;
 import java.util.function.BiFunction;
 
 /**
@@ -21,14 +22,24 @@ import java.util.function.BiFunction;
  *
  */
 @ControllerAdvice
+@Controller
 public class GlobalExceptionHandler {
     private final Logger logger= LoggerFactory.getLogger(this.getClass());
     private DefaultDataBufferFactory defaultDataBufferFactory=new DefaultDataBufferFactory();
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public Mono<Response> defaultErrorHandler(ServerHttpRequest req, Exception e)  {
+        if (e instanceof AccessDeniedException){
+            return Mono.just(ResponseUtils.accessDenied());
+        }
         logger.error("SystemError",e);
         logger.error("System Error remote:{} request path:{}",req.getRemoteAddress().getHostString(),req.getPath(),req.getQueryParams());
         return Mono.just(ResponseUtils.systemError());
+    }
+
+    @ResponseBody
+    @RequestMapping("**")
+    public Mono<Response> statusError(){
+        return Mono.just(ResponseUtils.notFound());
     }
 }
