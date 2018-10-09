f18e225a71ca6c8419fbecbb719d3bda09259466
xiezhongtao <example@email.com>
2018-10-09 15:07:54 +0800

    增加错误码

--- src/main/java/com/gzsf/operation/ResponseUtils.java
+++ src/main/java/com/gzsf/operation/ResponseUtils.java
@@ -7,8 +7,10 @@ import com.gzsf.operation.bean.Response;
 public class ResponseUtils {
     public static <T> Response<T> success(T data){return new Response<T>(0,"Ok",data);}
     public static <T> Response<T> noUser(){return new Response<T>(100,"No User");}
+    public static <T> Response<T> recordExists(){return new Response<T>(300,"Record Exits");}
     public static <T> Response<T> accessDenied(){return new Response<T>(403,"Access Denied");}
     public static <T> Response<T> systemError(){return new Response<T>(500,"System Error");}
+    public static <T> Response<T> paramError(){return new Response<T>(501,"Parameter Error");}
     public static <T> Response<T> notFound(){return new Response<T>(404,"Not Found");}
     public static <T> Response<T> notLogin(){return new Response<T>(101,"Not Login");}
     public static <T>PageResponse<T> successPage(Page<T> data){return new PageResponse<T>(0,"OK",data);}
